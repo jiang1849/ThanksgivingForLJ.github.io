0bf451eee26e7d69f10d43f664e7884671a3afb6
jiang1849 <weichenjiang0518@gmail.com>
2022-11-06 20:59:47 -0500

    Add files via upload

---
+++ js/talk.js
@@ -0,0 +1,20 @@
+new TypeIt("#lywry", {
+        loop: true,
+        cursorSpeed: 1000,
+        speed: 100
+    })
+    .type("亮晶晶")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("感恩遇到你，感恩节快乐")
+    .pause(3000)
+    .go();
+
+new TypeIt('#talkToWRY', {
+    lifeLike: true,
+    cursorSpeed: 1000,
+    waitUntilVisible: true,
+    speed: 100
+}).go();
